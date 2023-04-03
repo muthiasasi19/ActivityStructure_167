1bfdcec2a3bd3d5327636536ad8c4ec863e6d420
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-04-03 01:14:40 -0700

    acitivity structure 2

--- ActivityStructure2_167/ActivityStructure2_167.cpp
+++ ActivityStructure2_167/ActivityStructure2_167.cpp
@@ -8,3 +8,34 @@ struct AlamatDetail
 	string kota;
 
 };
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
+
+int main() {
+
+	Mahasiswa mhs;
+
+	cout << "Masukkan Nim: ";
+	cin >> mhs.nim;
+
+	cout << "Masukkan nama: ";
+	cin >> mhs.nama;
+	cout << "Alamat: ";
+	cout <<"\tNama desa: ";
+	cin >> mhs.alamat.desa;
+	cout << "\tNama kota: ";
+	cin >> mhs.alamat.desa;
+	cout << "Masukkan umur: ";
+	cin >> mhs.umur;
+
+	cout << "\nNim :" << mhs.nim;
+	cout << "\nnama :" << mhs.nama;
+	cout << "\ndesa :" << mhs.alamat.desa;
+	cout << "\nkota :" << mhs.alamat.kota;
+	 
+}
