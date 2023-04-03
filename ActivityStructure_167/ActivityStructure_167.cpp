7127fb6e23a502e8aab54ffb55b45c328c5ff689
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-04-03 00:28:10 -0700

    membuat variabel

--- ActivityStructure_167/ActivityStructure_167.cpp
+++ ActivityStructure_167/ActivityStructure_167.cpp
@@ -8,3 +8,13 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20210140011";
+	mhs1.nama = "Tasya";
+	mhs1.alamat = "Semarang";
+	mhs1.umur = 20;
+}
